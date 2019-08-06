e43f3c0cffbe57b4586b60525631cb71c3b40b47
高天阳 <13683265113@163.com>
2019-08-06 15:27:49 +0800

    首页 模块2、3 第一版

--- src/views/home.vue
+++ src/views/home.vue
@@ -12,7 +12,7 @@
       </i-col>
     </i-row>
     <i-row class="main">
-      <i-col>
+      <i-col class="w1200">
         <i-row>
           <i-col class="item1">
             <div class="content" :style="{top: contentTop}">
@@ -32,14 +32,25 @@
           </i-col>
         </i-row>
         <i-row>
-          <i-col>模块1</i-col>
+          <i-col class="item2">
+            <div class="content">
+              <div>自2016年开始接触并正式进入互联网这个行业，并深深的爱上了这个行业。在工作的3年来，做过前端开发，做过项目经理，在不同的岗位有着不同的收获。</div>
+            </div>
+            <div class="bgBox"></div>
+            <div class="imgList">
+              <div class="img"></div>
+              <div class="img"></div>
+              <div class="img"></div>
+              <div class="img"></div>
+            </div>
+          </i-col>
         </i-row>
-        <i-row>
-          <i-col>模块3</i-col>
-        </i-row>
-        <i-row>
-          <i-col>模块4</i-col>
-        </i-row>
+      </i-col>
+      <i-col class="item3">
+        <div class="content">
+          <div>写过<span style="color: #f8945d">响应式</span>，<span style="color: #42c4fb">移动端</span>，<span style="color: #f5ef4b">pc端</span>。</div>
+          <div>功能上来说，管理后台，考试评测系统，企业官网，活动宣传，可视化，地图等都有所涉猎。</div>
+        </div>
       </i-col>
     </i-row>
     <i-row class="bg">
@@ -133,69 +144,132 @@ export default {
     font-size: 20px;
   }
   .main {
-    .item1 {
-      position: relative;
-      height: 1000px;
-      .content {
+    .w1200 {
+      margin: 0 auto 50px;
+      width: 1200px;
+      .item1 {
         position: relative;
-        padding: 200px 100px;
-        top: 0;
-        width: 44vw;
-        height: 40vw;
-        background: #79a4ce;
-        font-size: 36px;
-        color: #fff;
-        .title {
-          margin-bottom: 30px;
-          font-size: 50px;
-          font-weight: 600;
+        height: 1000px;
+        .content {
+          position: relative;
+          padding: 140px 100px;
+          top: 0;
+          width: 50%;
+          height: 550px;
+          background: #79a4ce;
+          font-size: 28px;
+          color: #fff;
+          .title {
+            margin-bottom: 30px;
+            font-size: 40px;
+            font-weight: 600;
+          }
+        }
+        .imgList {
+          position: absolute;
+          width: 55%;
+          height: 700px;
+          top: 200px;
+          left: 45%;
+          .img {
+            float: left;
+            width: 300px;
+            height: 200px;
+            line-height: 350px;
+            text-align: center;
+            background: #61b1f6;
+          }
+          .imgList-left {
+            width: 300px;
+            float: left;
+            margin-right: 50px;
+            .img:nth-child(1) {
+              margin-bottom: 50px;
+              height: 300px;
+              background: url("../assets/img/img3.jpeg") no-repeat center center / cover;
+            }
+            .img:nth-child(2) {
+              background: url("../assets/img/img4.jpeg") no-repeat center -15px / cover;
+            }
+          }
+          .imgList-right {
+            width: 300px;
+            float: left;
+            .img:nth-child(1) {
+              background: url("../assets/img/img1.jpeg") no-repeat center -65px / cover;
+            }
+            .img:nth-child(2) {
+              margin-top: 50px;
+              height: 300px;
+              background: url("../assets/img/img2.jpeg") no-repeat center center / cover;
+            }
+          }
         }
       }
-      .imgList {
-        position: absolute;
-        width: 1050px;
-        height: 700px;
-        top: 200px;
-        left: 42vw;
-        .img {
-          float: left;
-          width: 500px;
-          height: 400px;
-          line-height: 350px;
-          text-align: center;
-          background: #61b1f6;
+      .item2 {
+        position: relative;
+        height: 900px;
+        .content {
+          position: relative;
+          padding: 20px 50px;
+          top: 0;
+          width: 55%;
+          height: 200px;
+          font-size: 28px;
+          color: #000;
+          background: #ffffff;
         }
-        .imgList-left {
-          width: 500px;
-          float: left;
-          margin-right: 50px;
+        .bgBox {
+          position: relative;
+          top: -250px;
+          left: 60%;
+          width: 750px;
+          height: 750px;
+          background: #2f778e;
+        }
+        .imgList {
+          position: absolute;
+          width: 100%;
+          height: 600px;
+          top: 260px;
+          .img {
+            float: left;
+            margin-right: 10px;
+            width: 290px;
+            height: 500px;
+            line-height: 350px;
+            text-align: center;
+            background: #61b1f6;
+          }
           .img:nth-child(1) {
-            margin-bottom: 50px;
-            height: 300px;
-            background: url("../assets/img/img3.jpeg") no-repeat center center / cover;
+            background: url("../assets/img/img4.jpeg") no-repeat center center / cover;
           }
           .img:nth-child(2) {
-            background: url("../assets/img/img4.jpeg") no-repeat center -30px / cover;
+            background: url("../assets/img/img4.jpeg") no-repeat center center / cover;
           }
-        }
-        .imgList-right {
-          width: 500px;
-          float: left;
-          .img:nth-child(1) {
-            background: url("../assets/img/img1.jpeg") no-repeat center -40px / cover;
+          .img:nth-child(3) {
+            background: url("../assets/img/img4.jpeg") no-repeat center center / cover;
           }
-          .img:nth-child(2) {
-            margin-top: 50px;
-            height: 300px;
-            background: url("../assets/img/img2.jpeg") no-repeat center center / cover;
+          .img:nth-child(4) {
+            background: url("../assets/img/img4.jpeg") no-repeat center center / cover;
           }
         }
       }
     }
-  }
-  .bg {
-    height: 2370px;
-    background: url("../assets/img/bg.png") no-repeat center center / cover;
+    .item3 {
+      width: 100vw;
+      height: 200px;
+      background: linear-gradient(to right, #890bbf, #0e0395);
+      .content {
+        margin: 0 auto;
+        padding: 50px 0;
+        top: 0;
+        width: 1200px;
+        height: 200px;
+        font-size: 28px;
+        color: #fff;
+      }
+    }
   }
   .w1200 {
     margin: 50px auto;
